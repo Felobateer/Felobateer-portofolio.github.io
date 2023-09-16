faf734b6031996951b9b93e7a7b3e1dcd8189b47
Felobateer <felobateersamy01@gmail.com>
2023-09-16 09:02:56 +0200

    add email sender

---
+++ server/email.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const email = express();
+const port = 3000;
+
+email.use(cors());
+email.use(bodyParser.urlencoded({ extended: false }));
+email.use(bodyParser.json());
+
+email.post("/", async (req, res) => {
+  const sender = req.body.sender;
+  const text = req.body.text;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "felobateersaadalla@gmail.com",
+      pass: "fyffcpksehqipiig",
+    },
+  });
+
+  const mailOptions = {
+    from: sender,
+    to: "felobateersaadalla@gmail.com",
+    subject: "email from portfolio",
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Email not sent");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+email.listen(port, () =>
+  console.log(`Server is running on https://felobateer-saadalla.netlify.app/`)
+);
